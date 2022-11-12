6a6bd7f085e151d6a351ab57f6362fdf131a6b2c
suriyaganesh97 <suriyaganesh97@gmail.com>
2022-11-12 19:13:12 +0530

    timer through rxjs

--- Ticket Booking/FrontEnd/src/app/timezonepage/timezonepage.component.ts
+++ Ticket Booking/FrontEnd/src/app/timezonepage/timezonepage.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { DatePipe } from '@angular/common';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { timer, Subscription,share } from 'rxjs';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-timezonepage',
   templateUrl: './timezonepage.component.html',
@@ -7,16 +8,29 @@ import { DatePipe } from '@angular/common';
 })
 export class TimezonepageComponent implements OnInit {
  date:any;
-
-  constructor(public datepipe: DatePipe) {
+ time = new Date();
+  rxTime = new Date();
+  intervalId;
+  subscription: Subscription;
+  constructor() {
    }
 
   ngOnInit(): void {
-    this.getTime();
+    // Using RxJS Timer
+    this.subscription = timer(0, 1000)
+      .pipe(
+        map(() => new Date()),
+        share()
+      )
+      .subscribe(time => {
+        this.rxTime = time;
+      });
   }
-getTime(){
-  this.date=new Date();
-let latest_date =this.datepipe.transform(this.date, 'dd-MM-yyyy');
-console.log(latest_date)
-}
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
 }
