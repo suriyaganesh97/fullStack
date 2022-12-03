548bff68fe05febbe9a8e70c6fa06f0f609e9f5a
suriyaganesh97 <suriyaganesh97@gmail.com>
2022-12-03 18:39:19 +0530

    initial commit

---
+++ Simple Projects/portfolio/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'portfolio';
+  constructor(private modalService: NgbModal) {
+  }
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
+}
