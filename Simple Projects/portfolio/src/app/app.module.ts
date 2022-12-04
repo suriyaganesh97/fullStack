fb583ffcabd90c7c8c1b18f064b1e9cc55d5a40e
suriyaganesh97 <suriyaganesh97@gmail.com>
2022-12-04 18:52:25 +0530

    adding routing

--- Simple Projects/portfolio/src/app/app.module.ts
+++ Simple Projects/portfolio/src/app/app.module.ts
@@ -5,7 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomepageComponent } from './homepage/homepage.component';
-
+import { Routes,RouterModule } from '@angular/router';
+import { IntroComponent } from './intro/intro.component';
+import { ExperienceComponent } from './experience/experience.component';
+const appRoutes: Routes = [
+  {path:'', component: HomepageComponent},
+  {path:'users', component: IntroComponent},
+  {path:'servers', component: ExperienceComponent},
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,6 +22,7 @@ import { HomepageComponent } from './homepage/homepage.component';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
