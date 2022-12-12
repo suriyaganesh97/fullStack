b7b112dbf79051fa22b5924e736357b5fb2a9eb8
suriyaganesh97 <suriyaganesh97@gmail.com>
2022-12-12 07:17:06 +0530

    smooth scroll fix

--- Simple Projects/portfolio/src/app/header/header.component.ts
+++ Simple Projects/portfolio/src/app/header/header.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit{
+active_class: any;
+  ngOnInit() {
+    let active_class="active"
+  }
+  scrollPointContact(contact_point:HTMLElement){
+    contact_point.scrollIntoView({behavior: "smooth"});
+  }
 }
