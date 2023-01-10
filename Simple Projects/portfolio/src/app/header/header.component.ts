321c76f13d8fcfb6e4dff0716fced1389efd65c4
suriyaganesh97 <suriyaganesh97@gmail.com>
2023-01-10 07:09:36 +0530

    fix for navbar issue

--- Simple Projects/portfolio/src/app/header/header.component.ts
+++ Simple Projects/portfolio/src/app/header/header.component.ts
@@ -8,6 +8,7 @@ import { Component,OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit{
 
   isActive:String | undefined;
+  isCollapsed = false;
   ngOnInit() {
     this.isActive='home';
     
