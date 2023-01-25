675dfe7326510fac404a699ee3bd2458988d00dc
suriyaganesh97 <suriyaganesh97@gmail.com>
2023-01-25 07:36:44 +0530

    blog backend commit

---
+++ Blog/blog_backend/blogsite/src/main/java/com/blog/blogsite/BlogsiteApplication.java
@@ -0,0 +1,13 @@
+package com.blog.blogsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogsiteApplication.class, args);
+	}
+
+}
---
+++ Blog/blog_backend/blogsite/src/test/java/com/blog/blogsite/BlogsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.blogsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
