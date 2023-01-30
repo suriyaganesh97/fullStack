00676e8d9abfe2ccd22284b1e8aa2e88742be3cc
suriyaganesh97 <suriyaganesh97@gmail.com>
2023-01-30 07:04:08 +0530

    adding signin controller

--- Ticket Booking/ticketbooking/src/main/java/com/backend/ticketbooking/signincontroller/Signin.java
+++ Ticket Booking/ticketbooking/src/main/java/com/backend/ticketbooking/signincontroller/Signin.java
@@ -1,11 +1,15 @@
 package com.backend.ticketbooking.signincontroller;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.apache.logging.log4j.Logger;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class Signin {
     static Logger logger =org.apache.logging.log4j.LogManager.getLogger((Signin.class));
@@ -25,5 +29,21 @@ if(encoder.matches("myPassword", result)){
 }
 		return "Home";
 	}
+
+	boolean isPasswordMatching=false;
+	String nameInBackend="suriya";
+	String passwordInBackend="Password";
+	@RequestMapping("/login")
+	public ResponseEntity<HttpStatus> logIn(@RequestParam String name,@RequestParam String password) {
+		logger.debug(name);
+		logger.debug(password);
+		logger.debug(nameInBackend);
+		logger.debug(passwordInBackend);
+		if(name.equals(nameInBackend) && password.equals(passwordInBackend)) {
+			return new ResponseEntity<>( HttpStatus.OK);
+		} 
+			return new ResponseEntity<>( HttpStatus.NOT_FOUND);
+
+}
 	
 }
