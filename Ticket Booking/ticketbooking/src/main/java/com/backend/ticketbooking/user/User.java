e9a56206c488ef5f5aa460d50e62253d7169eb94
suriyaganesh97 <suriyaganesh97@gmail.com>
2023-01-31 07:21:18 +0530

    adding auto generated id for user id

--- Ticket Booking/ticketbooking/src/main/java/com/backend/ticketbooking/user/User.java
+++ Ticket Booking/ticketbooking/src/main/java/com/backend/ticketbooking/user/User.java
@@ -1,10 +1,13 @@
 package com.backend.ticketbooking.user;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int userId;
     private String userName;
     private String password;
